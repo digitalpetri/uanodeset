2a59c06e7877ca59c4bfed87de15136e994340ee
Kevin Herron <kevinherron@gmail.com>
2025-11-20 09:14:16 -0800

    Add Spotless plugin for code formatting

--- uanodeset-namespace/src/main/java/module-info.java
+++ uanodeset-namespace/src/main/java/module-info.java
@@ -4,10 +4,8 @@ module com.digitalpetri.opcua.uanodeset.namespace {
   requires transitive com.digitalpetri.opcua.uanodeset.core;
   requires transitive org.eclipse.milo.opcua.stack.core;
   requires transitive org.eclipse.milo.opcua.sdk.server;
-  
   requires org.eclipse.milo.opcua.stack.encoding.xml;
   requires org.eclipse.milo.opcua.sdk.core;
-  
   requires jakarta.xml.bind;
   requires org.jspecify;
   requires org.slf4j;
